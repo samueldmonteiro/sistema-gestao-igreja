c0d51733624bd85c91b25bb87f3fb5d625b5bde8
Samuel Monteiro <samuel.dvmonteiro@gmail.com>
2025-02-08 14:39:58 -0300

    vite config domain

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: [
+      'adcampodaponte.site', // Adicione seu domínio aqui
+      'www.adcampodaponte.site', // Adicione também a versão com www, se necessário
+    ],
+  },
 })
